f5503dc9d7742187a58ef3e618f496ce74a54cf9
Sotatek-TuyenHa <tuyen.ha@sotatek.com>
2025-03-06 09:37:22 +0700

    feat: update payload format from queue

--- lambdas/src/index.ts
+++ lambdas/src/index.ts
@@ -1,14 +1,26 @@
 import { createCrawler } from './crawlers/CrawlerFactory';
 import { createParser } from './parsers/ParserFactory';
 
-export const crawl = async function (data: any) {
-  const crawler = createCrawler(data.platform);
-  const response = await crawler.crawl(data.payload);
+export const crawl = async function (event: any) {
+  const promises = event.Records.map((record: any) => {
+    const data = JSON.parse(record.body);
+
+    const crawler = createCrawler(data.platform);
+    return crawler.crawl(data.payload);
+  });
+
+  const response = await Promise.all(promises);
   return response;
 };
 
-export const parse = async function (data: any) {
-  const parser = createParser(data.platform);
-  const response = await parser.parse(data.payload);
+export const parse = async function (event: any) {
+  const promises = event.Records.map((record: any) => {
+    const data = JSON.parse(record.body);
+
+    const parser = createParser(data.platform);
+    return parser.parse(data.payload);
+  });
+
+  const response = await Promise.all(promises);
   return response;
 };
